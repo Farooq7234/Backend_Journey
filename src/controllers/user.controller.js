f2c2dd5b4b8b1f7db6529d3f12e7b6369cbff718
Umar Farooq <umarfarooq73970@gmail.com>
2024-07-08 15:30:44 +0530

    cookie is set on login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -108,7 +108,31 @@ const loginUser =  asyncHandler(async(req, res)=>{
     }
 
     const {accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in successfully!!"
+        )
+    )
 })
 
 
+// const logoutUser = asyncHandler(async(req, res)=>{
+
+// })
+
+ 
 export { registerUser }
